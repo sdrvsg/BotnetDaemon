b89979a8c173846ce38eaceef7ac7bfafe4ef2de
ONYYYX <onyxservers@yandex.ru>
2020-05-01 23:56:52 +0400

    Test purse

--- blueprints/gateway.py
+++ blueprints/gateway.py
@@ -1,5 +1,5 @@
 from hashlib import md5
-from flask import Blueprint, render_template, redirect, abort, request, make_response
+from flask import Blueprint, render_template, redirect, abort, request, make_response, url_for
 from flask_login import login_required, current_user
 from database import session
 from models.user import User
@@ -17,6 +17,12 @@ SECRET_TWO = 'p1hhfqfs'
 def payment():
     form = PaymentForm()
     if form.validate_on_submit():
+        if form.test:
+            current_user.money += form.money_amount.data
+            connect = session.create_session()
+            connect.merge(current_user)
+            connect.commit()
+            return redirect(url_for('gateway.success'))
         sign = md5(f'{SHOP_ID}:{form.money_amount.data}:{SECRET_ONE}:{current_user.id}'.encode('utf-8'))
         return redirect(f'{HOST}?m={SHOP_ID}&oa={form.money_amount.data}&o={current_user.id}&s={sign}')
     return render_template('gateway/payment.html', title='Пополнение баланса', form=form)
--- forms/payment.py
+++ forms/payment.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField
+from wtforms import IntegerField, SubmitField, HiddenField
 from wtforms.validators import DataRequired, NumberRange
 
 
 class PaymentForm(FlaskForm):
+    test = HiddenField('test', default=0)
     money_amount = IntegerField('Укажите сумму', validators=[DataRequired(), NumberRange(10)])
     submit = SubmitField('Пополнить')
