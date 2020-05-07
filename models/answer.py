ab196cdd6e077c690de1d54e80e94127bb577a48
ONYYYX <onyxservers@yandex.ru>
2020-05-07 22:33:53 +0400

    Final version (Refactor code, review code)

--- models/answer.py
+++ models/answer.py
@@ -7,7 +7,7 @@ class Answer(SqlAlchemyBase):
     __tablename__ = 'answers'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    question = sqlalchemy.Column(sqlalchemy.String(128), unique=True, nullable=False)
+    question = sqlalchemy.Column(sqlalchemy.String(128), nullable=False)
     answer = sqlalchemy.Column(sqlalchemy.String(128), nullable=False)
     bot_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('bots.id'), nullable=False)
     bot = orm.relationship('Bot', back_populates='answers', lazy='subquery')
